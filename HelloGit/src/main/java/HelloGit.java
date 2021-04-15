c7b9cd809ae4e81250eb0d2ef175956d350e9576
cuifua <1356114236@qq.com>
2021-04-15 10:19:03 +0800

    再次更改路径以后再试一下能不能成功

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit
         System.out.println("又在线更新");
         System.out.println("又一次在线更新");
         System.out.println("更改路径以后再试一下能不能成功");
+        System.out.println("再次更改路径以后再试一下能不能成功");
     }
 }
